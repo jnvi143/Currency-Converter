7676e72a33d6cadd87cd8efc47df42263f17eb9c
Janvi verma <jv6984483@gmail.com>
2026-09-03 01:38:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//acccesing elements 
+let fetchAPI = document.querySelector("#fetch-API")
+let useramount=document.querySelector("#amount")
+let options=document.querySelectorAll(".option")
+let convert=document.querySelectorAll("#convert")
+let result=document.querySelector("#result")
+
+
+//fetching API keys 
+
+let APIStore;
+let url = "https://api.twelvedata.com/currency_conversion?symbol=USD/INR&amount=100&apikey=5e0277b5e891419aa13d3ece9f78cb4a";
+let promise = fetch(url)
+ promise.then((response)=>{
+    return response.json()
+
+}).then((data)=>{
+   APIStore=(data.rate);
+   console.log(APIStore)
+    fetchAPI.innerText=`1USD =${APIStore} INR`
+})
+    
+
+
+//adding event listener to options
+let userChoice = "";
+options.forEach((option)=>{
+option.addEventListener("click",()=>{
+    option.style.backgroundColor="grey"
+    userChoice=option.innerText
+    console.log(userChoice)
+
+});
+});
+
+//convert
+convert.forEach((convert)=>{
+    convert.addEventListener("click",()=>{
+
+    //userenteredAmount
+    let amount=Number(useramount.value)
+    
+
+
+    //formula
+ if(userChoice==="INR-USD"){
+    finalresult=amount/APIStore;
+    console.log(finalresult)
+ }
+else{
+    finalresult=APIStore*amount;
+    console.log(finalresult)
+ }
+ result.innerText=finalresult;
+
+//reset all options button color
+options.forEach((option)=>{
+ option.style.backgroundColor="";
+ 
+  
+})
+   userChoice="";
+
+ });
+})
+ 
